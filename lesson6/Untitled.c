99f7a233f9c3adc766b7e09dd76fc932be7b62ca
BabyDragonPRO <romanus.terzi@gmail.com>
2018-11-18 11:56:54 +0000

    added tisting string reader program(begining the 6th lesson)

---
+++ lesson6/Untitled.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string start = get_string("What is your name? <Dan, Roma>");
+
+    /* switch(start)
+    {
+        case 'Dan':
+            char char1 = get_char("Do you eat shit? <y, n>");
+            switch(char1){
+                case 'y':
+                    printf("Of course you debilous , strange and stupid child...")
+            }
+            break;
+        case 'Roma':
+            printf("Hi, creator of this program");
+            char char1 = get_char("How are you?");
+            break;
+    } */
+    if (start == 'Dan'){
+        switch(start)
+        {
+            case 'Dan':
+                char char1 = get_char("Do you eat shit? <y, n>");
+                switch(char1){
+                    case 'y':
+                        printf("Of course you debilous , strange and stupid child...")
+                }
+        }
+    }
+}
