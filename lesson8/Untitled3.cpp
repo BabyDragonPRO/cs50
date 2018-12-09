7782626aa27fa3cdc9915a61e8ade383d1db6f44
BabyDragonPRO <romanus.terzi@gmail.com>
2018-12-09 13:57:57 +0000

    buble and selective sort

--- lesson8/Untitled3.cpp
+++ lesson8/Untitled3.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 
-using namespace std;
+ using namespace std;
 
-int binarySearch(int arr[], int l, int r, int x)
+int binarySearch (int arr[], int l, int r, int x)
 {
-    if(r >= l)
+    if (r >= l)
     {
         int mid = l + (r - l) / 2;
-        if ( arr[mid] == x)
+        if (arr[mid] == x) return mid;
+         if ( arr[mid] > x )
             return binarySearch(arr, l, mid-1, x);
         else
             return binarySearch(arr, mid+1, r, x);
     }
-
+    //element not found
     return -1;
 }
 
-int main(void)
+int main ()
 {
-    int array[] = {3, 4, 23, 42, 77, 100};
-
-    int n = sizeof(array)/sizeof(array[0]);
-
-    int x = 42;
-
-    int result = binarySearch(array, 0, n-1, x);
-
-    if(result == -1)
-        cout << "Element not found";
+    int arr[] = {3, 4, 23, 42, 77, 100};
+    //number of elements
+    int n = sizeof(arr)/sizeof(arr[0]);
+     int x = 10;
+     int result = binarySearch(arr, 0, n-1, x);
+     if(result == -1)
+        cout << "Element not found\n";
     else
-        cout << "Element found at index" << result;
-
+        cout << "Element found at index: " << result;
+    return 0;
 }
