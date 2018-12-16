0ee1428ed25a5446f6b3214312d650fa7aada366
BabyDragonPRO <romanus.terzi@gmail.com>
2018-12-16 13:55:08 +0000

    quicksort program

--- lesson10/quick.cpp
+++ lesson10/quick.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void printArray(int arr[], int size) {
- for (int i =0; i < size; i++)
+void QuickSort(int *arr, int left, int right) {
+ int l = left;
+ int r = right - 1;
+ int size = right - left;
+
+ if(size > 1) {
+  int pivot = arr[rand() % size + l];
+  while(l < r) {
+   while(arr[r] > pivot && r > l) {
+    r--;
+   }
+   while(arr[l] < pivot && l <= r) {
+    l++;
+   }
+   if(l < r) {
+    swap(arr[l], arr[r]);
+    l++;
+   }
+  }
+  QuickSort(arr, left, l);
+  QuickSort(arr, r, right);
+ }
+}
+
+//Create a random array
+int* makeRandomArray(int size) {
+ int* array = new int[size];
+ srand(time(NULL));
+ for (int i = 0; i < size; i++) {
+  array[i] = (100.0 * rand()) / RAND_MAX;
+ }
+ return array;
+}
+
+void printArray(int *arr, int size) {
+ for(int i = 0; i < size; i++) {
   cout << arr[i] << " ";
+ }
 }
 
-void swap(int *a,int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+int main() {
 
-int main(){
-    int count = 5;
-    int *p_count;
+ int size = 10;
 
-    p_count = &count;
+ int *array = makeRandomArray(size);
 
-    cout << p_count << endl;
-    cout << &cout << endl;
+ printArray(array, size);
+ cout << endl;
+
+ QuickSort(array, 0, size);
+
+ printArray(array, size);
+ delete [] array;
+
+ return 0;
 }
