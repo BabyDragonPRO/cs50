3ebfd8859605cd01de044d133264eecbe6c3d4d0
BabyDragonPRO <romanus.terzi@gmail.com>
2019-01-20 12:28:27 +0000

    Hell program, 11th lesson

--- lesson11/main.cpp
+++ lesson11/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int x;
+    string name = "";
+    cout << "Write your name:" << endl;
+    cin >> name;
+    cout << "Write how many times you visited hell:" << endl;
+    cin >> x;
+    if(name == "Danil" || name == "danil"){
+        x = 6666;
+    }
+    cout << "Welcome to hell, " << name << "." << endl << "You have visited hell " << x << " times." << endl;
+    return 0;
+}
