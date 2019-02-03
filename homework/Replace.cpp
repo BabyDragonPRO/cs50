c417cbb6de0ff7d33561b841a22c0ea9d72900aa
BabyDragonPRO <romanus.terzi@gmail.com>
2019-02-03 13:51:14 +0000

    lesson12

--- homework/Replace.cpp
+++ homework/Replace.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <vector>
 using namespace std;
-
-template<typename Container>
-bool is_quote(const Container& c, const string& s){
-    return search(c.begin(), c.end(), s.begin(), s.end()) != c.end();
-}
 
 int main () {
    std::string str="this is a test string.";
@@ -17,7 +10,6 @@ int main () {
    std::cout << str << '\n';
    str.replace(str.find(str3),str3.length(),str2);
 
-   //str.replace(npos,5,str2);
    std::cout << str << '\n';
    return 0;
 }
