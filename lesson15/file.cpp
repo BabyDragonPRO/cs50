3d8e0421f2b669e28ff5d6a5e80f989cc10a4406
BabyDragonPRO <romanus.terzi@gmail.com>
2019-03-31 13:08:09 +0000

    test programs

--- lesson15/file.cpp
+++ lesson15/file.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 
 int main(void){
+    string x;
+    cin >>  x;
     ofstream file;
     string name = "example.txt";
     char* y = new char[y, name.length() + 1];
     strcpy(y,name.c_str());
     file.open (y, ios::out);
     if(file.is_open()){
-        file << "Writing this to a file.\n";
+        file << "Writing\n";
     }
     file.close();
     return 0;
---
+++ test/inf_loops.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    bool Arg = true;
+    while(Arg == true){
+       cout << "A" << endl;
+    }
+}
